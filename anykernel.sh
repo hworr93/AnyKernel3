1b13c35a79bc35ab3e3f4bb83e3b1ef07c414281
hworr93 <hworr93@gmail.com>
2026-04-10 10:31:16 -0500

    Update kernel string and device names for Pixel 10

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sultan Kernel for the Pixel 9/Pro/Fold/XL
+kernel.string=Sultan Kernel for the Pixel 10/Pro/XL/Fold
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=caiman
-device.name2=komodo
-device.name3=tokay
-device.name4=comet
+device.name1=frankel
+device.name2=blazer
+device.name3=mustang
+device.name4=rango
 supported.versions=16
 supported.patchlevels=
 '; } # end properties
